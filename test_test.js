03fea96d30fcd945eb6c92c41e47744f34ff8c73
Casper Beyer <caspervonb@pm.me>
2021-07-22 17:54:49 +0800

    Add test tool

---
+++ test_test.js
@@ -0,0 +1,58 @@
+import { assertMatch } from "https://deno.land/std@0.102.0/testing/asserts.ts";
+
+const tests = [
+  {
+    input: ["testdata/test/pass.ts"],
+    output: "testdata/test/pass.out",
+  },
+  {
+    input: ["testdata/test/ignore.ts"],
+    output: "testdata/test/ignore.out",
+  },
+];
+
+for (const { input, output } of tests) {
+  Deno.test(`${input} => ${output}`, async function () {
+    const process = Deno.run({
+      env: {
+        "NO_COLOR": "1",
+      },
+      cmd: [
+        Deno.execPath(),
+        "run",
+        "--allow-all",
+        "--unstable",
+        "test.js",
+        "--headless",
+        "--browser",
+        "chrome",
+        ...input,
+      ],
+      stdout: "piped",
+      stderr: "null",
+    });
+
+    const actual = new TextDecoder().decode(await process.output())
+      .replaceAll(
+        "\r\n",
+        "\n",
+      );
+
+    const expected = new RegExp(
+      (await Deno.readTextFile(output))
+        .replaceAll("\r\n", "\n")
+        .replaceAll("(", "\\(")
+        .replaceAll(")", "\\)")
+        .replaceAll(".", "\\.")
+        .replaceAll("/", "\\/")
+        .replaceAll(
+          "[WILDCARD]",
+          ".+",
+        ),
+    );
+
+    process.close();
+
+    assertMatch(actual, expected);
+  });
+}
