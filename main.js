9d3652aff48194857436722c8db9d26b6fe22566
Casper Beyer <caspervonb@pm.me>
2021-07-22 18:12:38 +0800

    Create main.js

---
+++ main.js
@@ -0,0 +1,23 @@
+const commands = {
+  "test": await import("./test.js"),
+};
+
+try {
+  const args = Array.from(Deno.args);
+  if (args.length == 0) {
+    throw new Error(`No command specified`);
+  }
+
+  const name = args.shift();
+  const command = commands[name];
+  if (!command) {
+    throw new Error(`Unknown command ${name}`);
+  }
+
+  command.default(args);
+} catch (error) {
+  await Deno.writeAll(
+    Deno.stderr,
+    new TextEncoder().encode(`error: ${error.message}`),
+  );
+}
